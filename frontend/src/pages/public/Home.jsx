4efb0ca686c891278247df225efaf231e7b743f4
Mohammad Hazeem <Mohammad@3axis.se>
2025-09-25 01:47:22 +0200

    Home page

---
+++ frontend/src/pages/public/Home.jsx
@@ -0,0 +1,15 @@
+import Hero from "../../components/Hero/Hero.jsx";
+import ProductCardGrid from "../../components/ProductCardGrid/ProductCardGrid.jsx";
+import Spots from "../../components/Spots/Spots.jsx";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Spots />
+      <ProductCardGrid />
+    </>
+  );
+}
+
+export default Home;
